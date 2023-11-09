cd4bc73f3917eb1b1794af92b73d28e9ba6ab1ac
Mathéo Hersan <matheohersan@MacBook-Pro-de-Matheo.local>
2023-11-10 00:04:43 +0100

    initial commit

---
+++ Movie web v3.2.4/config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://movie.hersan-matheo.workers.dev/",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZDRmNjI3YzVkMTc5ZGRjYWYxNWIxNzkwZWQzMGM2MyIsInN1YiI6IjY1NGQ1ZmEzZDQ2NTM3MDEzODYwMzVhNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Rvj6FkYXhsikleI7vFMo9iTwnyuRHFIYCKimrvSiqF0"
+};
